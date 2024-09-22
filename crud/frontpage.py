148570791f1cf54269831a5e0ee75349a09433ea
Lovely Cunanan <70558552+CaptainPandaMonkey5@users.noreply.github.com>
2024-09-22 10:52:21 +0800

    updated crud
    - frontpage finished
    - contents wip

---
+++ crud/frontpage.py
@@ -0,0 +1,124 @@
+from PySide6.QtWidgets import QMainWindow, QMenu
+from PySide6.QtGui import QAction
+from ui_index import Ui_MainWindow
+
+class MySideBar(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle("Sidebar Menu")
+
+        self.icon_text_widget.setHidden(True)
+
+        self.students_dropdown.setHidden(True)
+        self.teachers_dropdown.setHidden(True)
+        self.finances_dropdown.setHidden(True)
+
+        self.dashboard_1.clicked.connect(self.switch_to_dashboard_page)
+        self.dashboard_2.clicked.connect(self.switch_to_dashboard_page)
+
+        self.institution_1.clicked.connect(self.switch_to_institution_page)
+        self.institution_2.clicked.connect(self.switch_to_institution_page)
+
+        self.student_info.clicked.connect(self.switch_to_studentInfo_page)
+        self.student_payment.clicked.connect(self.switch_to_studentPayments_page)
+        self.student_transaction.clicked.connect(self.switch_to_studentTransactions_page)
+
+        self.teacher_info.clicked.connect(self.switch_to_teacherInfo_page)
+        self.teacher_salaries.clicked.connect(self.switch_to_teacherSalaries_page)
+        self.teacher_transaction.clicked.connect(self.switch_to_teacherTransaction_page)
+
+        self.budgets.clicked.connect(self.switch_to_budgetInfo_page)
+        self.expenses.clicked.connect(self.switch_to_expensesInfo_page)
+        self.businessOverview.clicked.connect(self.switch_to_businessOverview_page)
+
+        self.settings_1.clicked.connect(self.switch_to_settings_page)
+        self.settings_2.clicked.connect(self.switch_to_settings_page) 
+
+        self.student_1.clicked.connect(self.students_context_menu)
+        self.teacher_1.clicked.connect(self.teachers_context_menu)
+        self.finance_1.clicked.connect(self.finances_context_menu)
+
+
+    def switch_to_dashboard_page(self):
+        self.stackedWidget.setCurrentIndex(0)  
+    def switch_to_institution_page(self):
+        self.stackedWidget.setCurrentIndex(1)
+    def switch_to_studentInfo_page(self):
+        self.stackedWidget.setCurrentIndex(2)
+    def switch_to_studentPayments_page(self):
+        self.stackedWidget.setCurrentIndex(3)
+    def switch_to_studentTransactions_page(self):
+        self.stackedWidget.setCurrentIndex(4)
+    def switch_to_teacherInfo_page(self):
+        self.stackedWidget.setCurrentIndex(5)
+    def switch_to_teacherSalaries_page(self):
+        self.stackedWidget.setCurrentIndex(6)
+    def switch_to_teacherTransaction_page(self):
+        self.stackedWidget.setCurrentIndex(7)
+    def switch_to_budgetInfo_page(self):
+        self.stackedWidget.setCurrentIndex(8)
+    def switch_to_expensesInfo_page(self):
+        self.stackedWidget.setCurrentIndex(9)
+    def switch_to_businessOverview_page(self):
+        self.stackedWidget.setCurrentIndex(10)
+    def switch_to_settings_page(self):
+        self.stackedWidget.setCurrentIndex(11)
+
+    def students_context_menu(self):
+        self.show_custom_context_menu(self.student_1, ["Student Information", "Student Payments", "Student Transaction"])
+    def teachers_context_menu(self):
+        self.show_custom_context_menu(self.teacher_1, ["Teacher Information", "Teacher Salaries", "Teacher Transaction"])
+    def finances_context_menu(self):
+        self.show_custom_context_menu(self.finance_1, ["Budgets", "Expenses", "Business Overview"])
+        #style for the menu
+    def show_custom_context_menu(self, button, menu_items):
+        menu = QMenu(self)
+
+        menu.setStyleSheet("""
+                           QMenu{
+                           background-color: black;
+                           color: white;
+                           }
+                           QMenu:selected {
+                           background-color: white;
+                           color: #12B298
+                           }
+                           """)
+
+        for item_text in menu_items:
+            action = QAction(item_text, self)
+            action.triggered.connect(self.handle_menu_item_click)
+            menu.addAction(action)
+
+        menu.move(button.mapToGlobal(button.rect().topRight()))
+        menu.exec()
+    def handle_menu_item_click(self):
+        text = self.sender().text()
+
+        if text == "Student Information":
+            self.switch_to_studentInfo_page()
+        elif text == "Student Payments":
+            self.switch_to_studentPayments_page()
+        elif text == "Student Transaction":
+            self.switch_to_studentTransactions_page()
+
+    def handle_menu_item_click(self):
+        text = self.sender().text()
+
+        if text == "Teacher Information":
+            self.switch_to_teacherInfo_page()
+        elif text == "Teacher Salaries":
+            self.switch_to_teacherSalaries_page()
+        elif text == "Teacher Transaction":
+            self.switch_to_teacherTransaction_page()
+
+    def handle_menu_item_click(self):
+        text = self.sender().text()
+
+        if text == "Budget":
+            self.switch_to_budgetInfo_page()
+        elif text == "Expenses":
+            self.switch_to_expensesInfo_page()
+        elif text == "Business Overview":
+            self.switch_to_businessOverview_page()

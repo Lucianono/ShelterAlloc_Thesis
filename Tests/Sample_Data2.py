06f550723400b06832599d01ffdeba09589b5072
Sam <angelcunanan15@gmail.com>
2025-02-01 17:44:23 +0800

    Chore: Added information

--- Tests/Sample_Data2.py
+++ Tests/Sample_Data2.py
@@ -1,6 +1,6 @@
 # sample data of communities with barangay names along with population and distances from each shelter
 Community = [
-    {"name": "Comm1", "population": 10, "maxdistance": 5, 
+    {"name": "Balite", "population": 0, "maxdistance": 1000, 
      "distances": {
         "Shel1": 3.61, 
         "Shel2": 2.83, 
@@ -8,9 +8,9 @@ Community = [
         "Shel4": 3.61}, 
     "portiontransfer":0.5,
     "portionwork" : {
-        "Work1" : 0.25
+        "Work1" : 0.67
     }},
-    {"name": "Comm2", "population": 10, "maxdistance": 5, 
+    {"name": "Balungao", "population": 0, "maxdistance": 1000, 
      "distances": {
         "Shel1": 3.16, 
         "Shel2": 2.23, 
@@ -18,9 +18,9 @@ Community = [
         "Shel4": 3.16}, 
     "portiontransfer":0.5,
     "portionwork" : {
-        "Work1" : 1
+        "Work1" : 0.67
     }},
-    {"name": "Comm3", "population": 10, "maxdistance": 5, 
+    {"name": "Buguion", "population": 0, "maxdistance": 1000, 
      "distances": {
         "Shel1": 3, 
         "Shel2": 2, 
@@ -28,9 +28,9 @@ Community = [
         "Shel4": 3}, 
     "portiontransfer":0.5,
     "portionwork" : {
-        "Work1" : 0.25
+        "Work1" : 0.67
     }},
-    {"name": "Comm4", "population": 10, "maxdistance": 5, 
+    {"name": "Bulusan", "population": 0, "maxdistance": 1000, 
      "distances": {
         "Shel1": 3.16, 
         "Shel2": 2.23, 
@@ -38,9 +38,9 @@ Community = [
         "Shel4": 3.16}, 
     "portiontransfer":0.5,
     "portionwork" : {
-        "Work1" : 0.25
+        "Work1" : 0.67
     }},
-    {"name": "Comm5", "population": 10, "maxdistance": 5, 
+    {"name": "Calizon", "population": 0, "maxdistance": 1000, 
      "distances": {
         "Shel1": 3.61, 
         "Shel2": 2.83, 
@@ -48,13 +48,253 @@ Community = [
         "Shel4": 3.61}, 
     "portiontransfer":0.5,
     "portionwork" : {
-        "Work1" : 0.25
+        "Work1" : 0.69
+    }},
+    {"name": "Calumpang", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Caniogan", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Corazon", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Frances", "population": 6, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.66
+    }},
+    {"name": "Gatbuca", "population": 115, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Gugo", "population": 57, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Iba Este", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.66
+    }},
+    {"name": "Iba O'Este", "population": 601, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.66
+    }},
+    {"name": "Longos", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Meysulao", "population": 56, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.65
+    }},
+    {"name": "Meyto", "population": 6, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.66
+    }},
+    {"name": "Palimbang", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Panducot", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.68
+    }},
+        {"name": "Pio Cruzcosa", "population": 92, "maxdistance": 5, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Poblacion", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.68
+    }},
+    {"name": "Pungo", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "San Jose", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "San Marcos", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.66
+    }},
+    {"name": "San Miguel", "population": 17, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Santa Lucia", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
+    }},
+    {"name": "Santo Niño", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.71
+    }},    
+    {"name": "Sapang Bayan", "population": 65, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.64
+    }},
+    {"name": "Sergio Bayan", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.64
+    }},
+    {"name": "Sucol", "population": 0, "maxdistance": 1000, 
+     "distances": {
+        "Shel1": 3.61, 
+        "Shel2": 2.83, 
+        "Shel3": 2.83,
+        "Shel4": 3.61}, 
+    "portiontransfer":0.5,
+    "portionwork" : {
+        "Work1" : 0.67
     }},
 ]
 
 # list of shelters with area1 and cost1 (area and cost as level 1 shelter), area 2 and cost2 (area and cost as level 2 shelter) 
 Shelters = [
-    {"name": "Shel1", "area1": 40, "cost1": 40, "area2": 40, "cost2": 40, 
+    {"name": "BMLTC Multi-Purpose Bldg and EC", "area1": 1641, "cost1": 0, "area2": 3282, "cost2": 98460000, 
      "distances": {
         "Shel1": 0, 
         "Shel2": 1, 
@@ -64,7 +304,7 @@ Shelters = [
     "distanceswork": {
          "Work1": 6.32
     }},
-    {"name": "Shel2", "area1": 40, "cost1": 40, "area2": 40, "cost2": 40, 
+    {"name": "F. Mendoza Memorial Elem Sch.", "area1": 1671, "cost1": 0, "area2": 3342, "cost2": 100260000, 
      "distances": {
         "Shel1": 1, 
         "Shel2": 0, 
@@ -74,7 +314,7 @@ Shelters = [
     "distanceswork": {
          "Work1": 5.39
     }},
-    {"name": "Shel3", "area1": 40, "cost1": 40, "area2": 40, "cost2": 40, 
+    {"name": "Calumpit Sports Complex", "area1": 947, "cost1": 0, "area2": 1894, "cost2": 56820000, 
      "distances": {
         "Shel1": 5, 
         "Shel2": 4, 
@@ -84,7 +324,187 @@ Shelters = [
     "distanceswork": {
          "Work1": 2.24
     }},
-    {"name": "Shel4", "area1": 40, "cost1": 40, "area2": 40, "cost2": 40, 
+    {"name": "Gatbuca Basketball Court", "area1": 376, "cost1": 0, "area2": 752, "cost2": 22560000, 
+     "distances": {
+        "Shel1": 6, 
+        "Shel2": 5, 
+        "Shel3": 1,
+        "Shel4": 0
+    },
+    "distanceswork": {
+         "Work1": 2
+    }},
+        {"name": "San Miguel Meysulao High School", "area1": 3464, "cost1": 0, "area2": 6928, "cost2": 207840000, 
+     "distances": {
+        "Shel1": 5, 
+        "Shel2": 4, 
+        "Shel3": 0,
+        "Shel4": 1
+    },
+    "distanceswork": {
+         "Work1": 2.24
+    }},
+    {"name": "Doña Damiana Elem School", "area1": 3135, "cost1": 0, "area2": 6270, "cost2": 188100000, 
+     "distances": {
+        "Shel1": 6, 
+        "Shel2": 5, 
+        "Shel3": 1,
+        "Shel4": 0
+    },
+    "distanceswork": {
+         "Work1": 2
+    }},
+    {"name": "Danga Dike", "area1": 126, "cost1": 0, "area2": 252, "cost2": 7560000, 
+     "distances": {
+        "Shel1": 5, 
+        "Shel2": 4, 
+        "Shel3": 0,
+        "Shel4": 1
+    },
+    "distanceswork": {
+         "Work1": 2.24
+    }},
+    {"name": "Meysulao Multipurpose/E.C.", "area1": 100, "cost1": 0, "area2": 200, "cost2": 6000000, 
+     "distances": {
+        "Shel1": 6, 
+        "Shel2": 5, 
+        "Shel3": 1,
+        "Shel4": 0
+    },
+    "distanceswork": {
+         "Work1": 2
+    }},
+    {"name": "Calizon Dike", "area1": 126, "cost1": 0, "area2": 252, "cost2": 7560000, 
+     "distances": {
+        "Shel1": 5, 
+        "Shel2": 4, 
+        "Shel3": 0,
+        "Shel4": 1
+    },
+    "distanceswork": {
+         "Work1": 2.24
+    }},
+    {"name": "San Marcos Elem. Sch.", "area1": 1147, "cost1": 0, "area2": 2294, "cost2": 68820000, 
+     "distances": {
+        "Shel1": 6, 
+        "Shel2": 5, 
+        "Shel3": 1,
+        "Shel4": 0
+    },
+    "distanceswork": {
+         "Work1": 2
+    }},
+        {"name": "San Marcos National H.S.", "area1": 6353, "cost1": 0, "area2": 12706, "cost2": 381180000, 
+     "distances": {
+        "Shel1": 5, 
+        "Shel2": 4, 
+        "Shel3": 0,
+        "Shel4": 1
+    },
+    "distanceswork": {
+         "Work1": 2.24
+    }},
+    {"name": "GMA Kapuso E.C.", "area1": 200, "cost1": 0, "area2": 400, "cost2": 12000000, 
+     "distances": {
+        "Shel1": 6, 
+        "Shel2": 5, 
+        "Shel3": 1,
+        "Shel4": 0
+    },
+    "distanceswork": {
+         "Work1": 2
+    }},
+    {"name": "NV9 Multi-Purpose", "area1": 2513, "cost1": 0, "area2": 5026, "cost2": 150780000, 
+     "distances": {
+        "Shel1": 5, 
+        "Shel2": 4, 
+        "Shel3": 0,
+        "Shel4": 1
+    },
+    "distanceswork": {
+         "Work1": 2.24
+    }},
+    {"name": "Frances E.C.", "area1": 150, "cost1": 0, "area2": 300, "cost2": 9000000, 
+     "distances": {
+        "Shel1": 6, 
+        "Shel2": 5, 
+        "Shel3": 1,
+        "Shel4": 0
+    },
+    "distanceswork": {
+         "Work1": 2
+    }},
+    {"name": "Balungao E.C.", "area1": 216, "cost1": 0, "area2": 432, "cost2": 12960000, 
+     "distances": {
+        "Shel1": 5, 
+        "Shel2": 4, 
+        "Shel3": 0,
+        "Shel4": 1
+    },
+    "distanceswork": {
+         "Work1": 2.24
+    }},
+    {"name": "Gugo E.C.", "area1": 336, "cost1": 0, "area2": 672, "cost2": 20160000, 
+     "distances": {
+        "Shel1": 6, 
+        "Shel2": 5, 
+        "Shel3": 1,
+        "Shel4": 0
+    },
+    "distanceswork": {
+         "Work1": 2
+    }},
+        {"name": "San Marcos E.C.", "area1": 40, "cost1": 0, "area2": 80, "cost2": 2400000, 
+     "distances": {
+        "Shel1": 5, 
+        "Shel2": 4, 
+        "Shel3": 0,
+        "Shel4": 1
+    },
+    "distanceswork": {
+         "Work1": 2.24
+    }},
+    {"name": "San Jose E.C.", "area1": 268, "cost1": 0, "area2": 536, "cost2": 16080000, 
+     "distances": {
+        "Shel1": 6, 
+        "Shel2": 5, 
+        "Shel3": 1,
+        "Shel4": 0
+    },
+    "distanceswork": {
+         "Work1": 2
+    }},
+        {"name": "MEYTO Multi-Purpose/ E.C.", "area1": 142, "cost1": 0, "area2": 284, "cost2": 8520000, 
+     "distances": {
+        "Shel1": 5, 
+        "Shel2": 4, 
+        "Shel3": 0,
+        "Shel4": 1
+    },
+    "distanceswork": {
+         "Work1": 2.24
+    }},
+    {"name": "Barangay Hall Bulusan", "area1": 700, "cost1": 0, "area2": 1400, "cost2": 42000000, 
+     "distances": {
+        "Shel1": 6, 
+        "Shel2": 5, 
+        "Shel3": 1,
+        "Shel4": 0
+    },
+    "distanceswork": {
+         "Work1": 2
+    }},
+        {"name": "Brgy. Hall Sta. Lucia", "area1": 150, "cost1": 0, "area2": 300, "cost2": 9000000, 
+     "distances": {
+        "Shel1": 5, 
+        "Shel2": 4, 
+        "Shel3": 0,
+        "Shel4": 1
+    },
+    "distanceswork": {
+         "Work1": 2.24
+    }},
+    {"name": "Mun. Covered Court", "area1": 713, "cost1": 0, "area2": 1426, "cost2": 42780000, 
      "distances": {
         "Shel1": 6, 
         "Shel2": 5, 
